814888fd0eab9a4c94585fd435b5f27d733c6919
韦博瀚 <41739624+bohanjun@users.noreply.github.com>
2022-04-17 17:05:19 +0800

    Update according to exlg.cc
    Co-authored-by: 汪心禾 <wangxinhe06@gmail.com>

--- js/night.js
+++ js/night.js
@@ -6,17 +6,38 @@ function openNightMode(){
 		nlist[i].className+=" night";
 	}
 }
-// window.onload=function(){
-// 	var d = new Date();
-// 	var nowhour = d.getHours();
-// 	if(nowhour>=10||nowhour<=6){
-// 		openNightMode();
-// 	}
-// }
 $(document).ready(function(){
 	var d = new Date();
 	var nowhour = d.getHours();
+	var nowmon = d.getMonth();
+	var nowday = d.getDate();
 	if(nowhour>=22||nowhour<=6){
 		openNightMode();
 	}
+	if(nowmon==11&&nowday==13){
+		document.querySelector("html").className+=" pray";
+	}
+	if(nowmon==1&&nowday==22){
+		document.querySelector(".bgimg").className+=" fascinated";
+	}
+	if(nowmon==2){
+		document.querySelector(".bgimg").className+=" sakural";
+	}
+	if(nowmon==8){
+		document.querySelector(".bgimg").className+=" fallal";
+	}
+	var baop=localStorage.getItem("extendluoguopenba");
+	if(baop=="1"){
+		try{
+			document.querySelector("p.maker-infor a").className+=" minstdfx";
+		}
+		catch{
+			document.querySelector("p.maker-info a").className+=" minstdfx";
+		}
+	}
 });
+
+function minstdfx(open){
+	//appear 0 disappear 1
+	localStorage.setItem("extendluoguopenba",open);
+}
